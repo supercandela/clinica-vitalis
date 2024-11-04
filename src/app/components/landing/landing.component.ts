22eeb62ac08f0270a965037f5e4c3cbeb255498e
supercandela <supercandela@gmail.com>
2024-11-03 21:25:13 -0300

    landing page

---
+++ src/app/components/landing/landing.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './landing.component.html',
+  styleUrl: './landing.component.scss'
+})
+export class LandingComponent {
+  logo: string = '../../../assets/isologo-vertical.png';
+  plans = [
+    { name: 'Plan Básico', description: 'Consultas generales y servicios básicos.', price: '$300/mes' },
+    { name: 'Plan Familiar', description: 'Cobertura para hasta 4 miembros de la familia.', price: '$500/mes' },
+    { name: 'Plan Premium', description: 'Atención prioritaria y acceso a todas las especialidades.', price: '$800/mes' }
+  ];
+}
