1e1ec41c1fd91c7257f68e5b8d41e5a2ff339e0d
supercandela <supercandela@gmail.com>
2024-11-05 17:17:05 -0300

    agrego página de login y de registro - no funcional

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,19 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
-
     {
         path: '',
         component: LandingComponent
+    },
+    {
+        path: 'registro',
+        component: RegistroComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
     }
 ];
--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
@@ -9,10 +10,23 @@ import { Component } from '@angular/core';
   styleUrl: './landing.component.scss'
 })
 export class LandingComponent {
-  logo: string = '../../../assets/isologo-vertical.png';
   plans = [
     { name: 'Plan Básico', description: 'Consultas generales y servicios básicos.', price: '$300/mes' },
     { name: 'Plan Familiar', description: 'Cobertura para hasta 4 miembros de la familia.', price: '$500/mes' },
     { name: 'Plan Premium', description: 'Atención prioritaria y acceso a todas las especialidades.', price: '$800/mes' }
   ];
+
+  constructor (
+    private router: Router
+  ) {
+
+  }
+
+  irALogin () {
+    this.router.navigateByUrl('/login');
+  }
+
+  irARegistro () {
+    this.router.navigateByUrl('/registro');
+  }
 }
