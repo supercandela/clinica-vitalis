ba18a037ed96e5904aae1a3fb341a9fb55ccf055
supercandela <supercandela@gmail.com>
2024-12-03 18:29:46 -0300

    [GRAFICOS] Grafico turnos solicitados

--- src/app/components/graficos/graficos.component.ts
+++ src/app/components/graficos/graficos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from "../header/header.component";
 
@@ -9,12 +9,13 @@ import * as XLSX from 'xlsx';
 
 import { TurnosService } from '../../services/turnos.service';
 import { Chart, registerables } from 'chart.js';
+import { FormsModule } from '@angular/forms';
 Chart.register(...registerables);
 
 @Component({
   selector: 'app-graficos',
   standalone: true,
-  imports: [HeaderComponent, CommonModule, SpinnerDirective ],
+  imports: [HeaderComponent, CommonModule, SpinnerDirective, FormsModule],
   templateUrl: './graficos.component.html',
   styleUrl: './graficos.component.scss'
 })
@@ -30,15 +31,38 @@ export class GraficosComponent implements OnInit, OnDestroy {
   turnosPorEspecialidadListado: any[] = [];
   subTurnosPorEspecialidad?: Subscription;
   espeCan: { especialidad: string; cantidad: number }[] = [];
-  labelData: string[] = [];
-  realData: number[] = [];
-  colorData: any[] = [];
+  labelDataTurnosEspecialidad: string[] = [];
+  realDataTurnosEspecialidad: number[] = [];
+  colorDataTurnosEspecialidad: any[] = [];
 
   turnosPorDia: boolean = false;
   turnosPorDiaListado: any[] = [];
   subTurnosPorDia?: Subscription;
   diaCan: { dia: string; cantidad: number }[] = [];
-  realDataPorDia: any[] = [];
+  labelDataTurnosPorDia: string[] = [];
+  realDataTurnosPorDia: any[] = [];
+  colorDataTurnosPorDia: any[] = [];
+
+  turnosSolicitadosPorMedico: boolean = false;
+  subTurnosSolicitadosPorMedico?: Subscription;
+  turnosSolicitadosPorMedicoListado: any[] = [];
+  labelDataTurnosPorMedico: string[] = [];
+  realDataTurnosPorMedico: any[] = [];
+  colorDataTurnosPorMedico: any[] = [];
+
+  cantidadesPorDia: { especialista: string; cantidad: number }[] = [];
+  fecha_desde!: string;
+  fecha_hasta!: string;
+  turnosFiltrados!: any[];
+  mensaje: string = "";
+  turnosMap!: any[];
+  chart: Chart | null = null;
+  canvaCreado: boolean = false;
+  estadoSolicitado!: string;
+  enterLeave = signal(true);
+  isOpen = signal(false);
+
+
 
   constructor (
     private authService: AuthService,
@@ -55,6 +79,8 @@ export class GraficosComponent implements OnInit, OnDestroy {
     this.seleccionLogs = true;
     this.turnosPorEspecialidad = false;
     this.turnosPorDia = false;
+    this.turnosSolicitadosPorMedico = false;
+
     this.isLoading = true;
     this.subLogsListado = this.authService.obtenerLogsUsuarios().subscribe((respuesta) =>{
       this.logsListado = respuesta;
@@ -76,6 +102,8 @@ export class GraficosComponent implements OnInit, OnDestroy {
     this.seleccionLogs = false;
     this.turnosPorEspecialidad = true;
     this.turnosPorDia = false;
+    this.turnosSolicitadosPorMedico = false;
+
     this.isLoading = true;
     this.subTurnosPorEspecialidad = this.turnosService.obtenerTodosLosTurnos().subscribe((respuesta: any) => {
       this.turnosPorEspecialidadListado = respuesta;
@@ -97,12 +125,11 @@ export class GraficosComponent implements OnInit, OnDestroy {
 
     if (this.espeCan !== null) {
       this.espeCan.map(o => {
-        this.labelData.push(o.especialidad);
-        this.realData.push(o.cantidad);
+        this.labelDataTurnosEspecialidad.push(o.especialidad);
+        this.realDataTurnosEspecialidad.push(o.cantidad);
       })
     }
-    console.log(this.labelData, this.realData);
-    this.cargarDatos(this.labelData, this.realData);
+    this.cargarDatos(this.labelDataTurnosEspecialidad, this.realDataTurnosEspecialidad);
   }
 
   cargarDatos(labelData: any, valuedata: any) {
@@ -130,6 +157,8 @@ export class GraficosComponent implements OnInit, OnDestroy {
     this.seleccionLogs = false;
     this.turnosPorEspecialidad = false;
     this.turnosPorDia = true;
+    this.turnosSolicitadosPorMedico = false;
+
     this.isLoading = true;
     this.subTurnosPorDia = this.turnosService.obtenerTodosLosTurnos().subscribe((respuesta: any) => {
       this.turnosPorDiaListado = respuesta;
@@ -145,10 +174,9 @@ export class GraficosComponent implements OnInit, OnDestroy {
       return contador;
     }, {} as { [fecha: string]: number });
 
-    this.labelData = Object.keys(this.diaCan);
-    this.realDataPorDia = Object.values(this.diaCan);
-    console.log(this.labelData, this.realDataPorDia);
-    this.cargarDatosPieChart(this.labelData, this.realData, 'piechart', 'doughnut');
+    this.labelDataTurnosPorDia = Object.keys(this.diaCan);
+    this.realDataTurnosPorDia = Object.values(this.diaCan);
+    this.cargarDatosPieChart(this.labelDataTurnosPorDia, this.realDataTurnosPorDia, 'piechart', 'doughnut');
   }
 
   cargarDatosPieChart(labelData: any, valuedata: any, chartId: string, chartType: any) {
@@ -170,6 +198,102 @@ export class GraficosComponent implements OnInit, OnDestroy {
 
       }
     })
+  }
+
+  seleccionarTurnosSolicitadosPorMedico () {
+    this.seleccionLogs = false;
+    this.turnosPorEspecialidad = false;
+    this.turnosPorDia = false;
+    this.turnosSolicitadosPorMedico = true;
+    this.estadoSolicitado = '';
+    this.isLoading = true;
+    this.subTurnosSolicitadosPorMedico = this.turnosService.obtenerTurnosConEspecialistaYPaciente().subscribe((respuesta: any) => {
+      this.turnosSolicitadosPorMedicoListado = respuesta;
+      this.conseguirCantidadesPorDia();
+      this.isLoading = false;
+    });
+  }
+
+
+
+
+  filtrarTurnos() {
+    if (!this.fecha_desde || !this.fecha_hasta) {
+      this.mensaje = 'Por favor, selecciona un rango de fechas válido';
+      return;
+    }
+
+    // Convertir las fechas ingresadas al formato Date
+    const desde = new Date(this.fecha_desde);
+    const hasta = new Date(this.fecha_hasta);
+
+    if (desde > hasta) {
+      this.mensaje = 'La fecha "Desde" no puede ser mayor que la fecha "Hasta"';
+      return;
+    }
+
+    if (this.estadoSolicitado === 'finalizado') {
+      // // Filtrar el array de turnosAll
+      // this.turnosFiltrados = this.turnosMap.filter(turno => {
+      //   const fechaTurno = this.convertirFecha(turno.fecha); // Convertir la fecha del turno a Date
+      //   return (
+      //     fechaTurno >= desde &&
+      //     fechaTurno <= hasta &&
+      //     turno.estado === 'finalizado' // Filtro por estado
+      //   );
+      // });
+    } else {
+      this.turnosFiltrados = this.turnosSolicitadosPorMedicoListado.filter(turno => {
+        const fechaTurno = turno.fecha.toDate();
+        return fechaTurno >= desde && fechaTurno <= hasta;
+      });
+    }
+
+    this.conseguirCantidadesTurnosPorMedico();
+    this.isOpen.set(!this.isOpen());
+  }
+
+  conseguirCantidadesTurnosPorMedico() {
+    this.cantidadesPorDia = this.turnosFiltrados.reduce((acc, turno) => {
+      const idEspecialista = turno.especialistaId;
+      acc[idEspecialista] = (acc[idEspecialista] || 0) + 1;
+    
+      return acc;
+    }, {} as Record<string, number>);
+
+    console.log(this.cantidadesPorDia);
+
+    this.labelDataTurnosPorMedico = Object.keys(this.cantidadesPorDia);
+    this.realDataTurnosPorMedico = Object.values(this.cantidadesPorDia);
+    this.cargarDatosGraficoTurnosPorMedico(this.labelDataTurnosPorMedico, this.realDataTurnosPorMedico, 'turnosPorMedico', 'polarArea');
+  }
+
+  cargarDatosGraficoTurnosPorMedico(labelData: any, valuedata: any, chartId: string, chartType: any) {
+    if (!this.canvaCreado) {
+      const mychar = new Chart(chartId, {
+        type: chartType,
+        data: {
+          labels: labelData,
+          datasets: [
+            {
+              label: 'Turnos',
+              data: valuedata,
+              backgroundColor: ['#c0392b', '#9b59b6', '#2980b9', '#1abc9c', '#d4ac0d', '#2e4053'],
+              borderColor: ['#c0392b', '#9b59b6', '#2980b9', '#1abc9c', '#d4ac0d', '#2e4053'],
+              borderWidth: 1
+            }
+          ]
+        },
+        options: {
+
+        }
+      })
+      this.canvaCreado = true;
+    }
+  }
+
+  limpiarMensaje() {
+    this.mensaje = "";
   }
 
   ngOnDestroy(): void {
