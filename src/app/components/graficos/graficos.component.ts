5297992c299dd7881d8f5ad29dbd6d31cb05fb3b
supercandela <supercandela@gmail.com>
2024-12-03 16:18:45 -0300

    [LOGS USUARIOS] Fix para fecha en descarga de excel

--- src/app/components/graficos/graficos.component.ts
+++ src/app/components/graficos/graficos.component.ts
@@ -2,14 +2,13 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from "../header/header.component";
 
-import * as XLSX from 'xlsx';
-import * as FileSaver from 'file-saver';
-
-import { Chart, registerables } from 'chart.js';
-import { Subscription } from 'rxjs';
-import { TurnosService } from '../../services/turnos.service';
-import { SpinnerDirective } from '../../directives/spinner.directive';
 import { AuthService } from '../../services/auth.service';
+import { SpinnerDirective } from '../../directives/spinner.directive';
+import { Subscription } from 'rxjs';
+import * as XLSX from 'xlsx';
+
+import { TurnosService } from '../../services/turnos.service';
+import { Chart, registerables } from 'chart.js';
 Chart.register(...registerables);
 
 @Component({
@@ -61,39 +60,15 @@ export class GraficosComponent implements OnInit, OnDestroy {
 
   }
 
-  exportAsExcel(fileName: string): void {
-    let encabezados = Object.keys(this.logsListado[0]);
-
-    const datosConvertidos = this.logsListado.map((dato) => ({
-      ...dato,
-      especialidades: Array.isArray(dato.especialidades) ? dato.especialidades.join(', ') : dato.especialidades,
-    }));
-
-    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet([]);
-    XLSX.utils.sheet_add_aoa(worksheet, [encabezados], {
-      origin: 'A1',
-    });
-    XLSX.utils.sheet_add_json(worksheet, datosConvertidos, {
-      origin: 'A2',
-      skipHeader: true,
-    });
-
-    const workbook: XLSX.WorkBook = {
-      Sheets: { data: worksheet },
-      SheetNames: ['data'],
-    };
-
-    const excelBuffer: any = XLSX.write(workbook, {
-      bookType: 'xlsx',
-      type: 'array',
-    });
-
-    this.guardarArchivoXLS(excelBuffer, fileName);
-  }
-
-  private guardarArchivoXLS(buffer: any, fileName: string): void {
-    const data: Blob = new Blob([buffer], { type: 'application/octet-stream' });
-    FileSaver.saveAs(data, `${fileName}.xlsx`);
+  exportAsExcel(): void {
+    const tabla = document.getElementById('tabla-logs-completa');
+    
+    if (tabla) {
+      const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(tabla);
+      const wb: XLSX.WorkBook = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(wb, ws, 'Logs');
+      XLSX.writeFile(wb, 'logs-usuarios.xlsx');
+    }
   }
 
   // conseguirCantidades() {
